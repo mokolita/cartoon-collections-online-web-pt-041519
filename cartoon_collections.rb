f3b6b6219b6031625a7545e0a3b1553424b88c28
Meg Galvez <meg@oakmaine.com>
2019-04-17 16:17:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -27,7 +27,7 @@ end
 def find_the_cheese(cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
   
-  if cheese.find {|type| 
+  if cheese.find {|type| cheese_types.include?(type)}
     return cheese_types
   else 
     nil 
